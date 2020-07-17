dbbb5b25a71f3784220b56b6a8638a8c6a073e97
Docker Container <madisetti@jhu.edu>
2020-07-17 17:18:05 +0000

    Daria Changes!

--- pd3_plot_colors.py
+++ pd3_plot_colors.py
@@ -8,7 +8,10 @@ from matplotlib import colors as mcolors
 from matplotlib.collections import LineCollection
 from matplotlib import collections as mc
 import pylab as pl
-
+import importlib
+import Graph_class
+importlib.reload(Graph_class)
+from Graph_class import Graph
 
 def normalize(a, b, c):
     """Creates a unit vector from a, b, c"""
@@ -16,61 +19,60 @@ def normalize(a, b, c):
     return vector / np.linalg.norm(vector)
 
 
-def create_graph(study, timestep, x_axis, y_axis, not_visited, node_vectors, node_vectors_2d, links):
-    """! \brief Creates a graph
+# def create_graph(study, timestep, x_axis, y_axis, not_visited, node_vectors, node_vectors_2d, links):
+#     """! \brief Creates a graph
     
-    Exports a graph from protobuf, and gets the edge and node information
-    \param Takes in a graph
-    \param Takes in x_axis and y_axis for plotting
-    \param Takes in a set which represents the nodes that have been visited
-    \param Takes in 2 lookup for node_id to to node_vector.
-    """
+#     Exports a graph from protobuf, and gets the edge and node information
+#     \param Takes in a graph
+#     \param Takes in x_axis and y_axis for plotting
+#     \param Takes in a set which represents the nodes that have been visited
+#     \param Takes in 2 lookup for node_id to to node_vector.
+#     """
         
-    graph = study.export_protobuf()
-    vertices = graph.state[timestep].nodes
-    edges = graph.state[timestep].links
-    g = {}
-#     links = { 
-#        node_a: { node_b: segment_info } }
-#     node
+#     graph = study.export_protobuf()
+#     vertices = graph.state[timestep].nodes
+#     edges = graph.state[timestep].links
+#     g = {}
+# #     links = { 
+# #        node_a: { node_b: segment_info } }
+# #     node
    
-    for edge in edges:
-        not_visited.add(edge.leading)
-        not_visited.add(edge.trailing)
-        leading_neighbors = g.get(edge.leading, [])
-        leading_neighbors.append(edge.trailing)
-        trailing_neighbors = g.get(edge.trailing, [])
-        trailing_neighbors.append(edge.leading)
-        g[edge.leading] = leading_neighbors
-        g[edge.trailing] = trailing_neighbors
+#     for edge in edges:
+#         not_visited.add(edge.leading)
+#         not_visited.add(edge.trailing)
+#         leading_neighbors = g.get(edge.leading, [])
+#         leading_neighbors.append(edge.trailing)
+#         trailing_neighbors = g.get(edge.trailing, [])
+#         trailing_neighbors.append(edge.leading)
+#         g[edge.leading] = leading_neighbors
+#         g[edge.trailing] = trailing_neighbors
 
-    for edge in edges: # (1, 2)
-        if edge.leading not in links:
-            links[edge.leading] = {}
+#     for edge in edges: # (1, 2)
+#         if edge.leading not in links:
+#             links[edge.leading] = {}
 
-        if edge.trailing not in links:
-            links[edge.trailing] = {}
+#         if edge.trailing not in links:
+#             links[edge.trailing] = {}
 
-        links[edge.leading][edge.trailing] = edge.slip
-        links[edge.trailing][edge.leading] = edge.slip
+#         links[edge.leading][edge.trailing] = edge.slip
+#         links[edge.trailing][edge.leading] = edge.slip
 
-    for node_id in vertices:
-        node = vertices[node_id]
-        node_3d = np.array([node.x, node.y, node.z])
-        node_vectors[node_id] = node_3d
+#     for node_id in vertices:
+#         node = vertices[node_id]
+#         node_3d = np.array([node.x, node.y, node.z])
+#         node_vectors[node_id] = node_3d
         
-        node_x = x_axis.dot(node_3d)
-        node_y = y_axis.dot(node_3d)
-        node_vectors_2d[node_id] = (node_x, node_y)
+#         node_x = x_axis.dot(node_3d)
+#         node_y = y_axis.dot(node_3d)
+#         node_vectors_2d[node_id] = (node_x, node_y)
 
-    return g
+#     return g
 
 def collect_and_plot(lines):
     """! \brief Collects data for lines and plots 
     
     Goes through the lines array and collects the x, y, and z values needed to plot and then plots those values
     \param Takes in an array of node information (x, y, z)
-    
     Returns: xs, ys, and zs which are points to plot on the graph
     """
     
@@ -98,7 +100,7 @@ def collect_and_plot(lines):
     
     return xs, ys, zs, x, y, zs
 
-def dfs(graph, node_vectors, not_visited, visited, links, color):
+def dfs(g, color):
     """! \brief Searches graph.
     
     Search the provided graph using depth first search, returns a bunch of lines.
@@ -113,32 +115,33 @@ def dfs(graph, node_vectors, not_visited, visited, links, color):
     color = []
     color_lookup = {0: "red", 1: "blue", 2: "green", 3: "purple", 4: "orange"}
     
-    def search(graph, node_vectors, current, previous, line, links, previous_slip, color):
+    def search(graph, current, previous, line, previous_slip, color):
         neighbors = graph[current]
-        here = node_vectors[current]
+        here = graph.node_vectors[current]
         branch = line
         branch.append(here)
+        print("running")
 
         first_visit = current not in visited
         end_of_line = (len(neighbors) == 1 and neighbors[0] == previous)
 
         visited.add(current)
-        if current in not_visited:
-            not_visited.remove(current)
+        if current in graph.not_visited:
+            graph.not_visited.remove(current)
 
         if not first_visit or end_of_line:
-            lines.append(branch)
+            graph.lines.append(branch)
             color.append(color_lookup[previous_slip])
             return
 
         first_iteration = True
         for node in neighbors:
-            slip = links[current][node]
+            slip = graph.links[current][node]
             if slip != previous_slip:
                 # What a hack!
                 # See if you can find a nicer way of doing this
                 if first_iteration:
-                    lines.append(branch)
+                    graph.lines.append(branch)
                     color.append(color_lookup[slip])
                 branch = [here]
                     
@@ -146,10 +149,11 @@ def dfs(graph, node_vectors, not_visited, visited, links, color):
                 search(graph, node_vectors, node, here, branch, links, slip, color)
                 branch = [here]
             first_iteration = False
-                
-    while len(not_visited) > 0:
+            
+    print(g.not_visited)
+    while len(g.not_visited) > 0:
         start_node = not_visited.pop()
-        search(graph, node_vectors, start_node, None, [], links, None, color)
+        search(graph, start_node, None, [], None, color)
         
     return lines, color         
                 
@@ -178,7 +182,7 @@ def plot_study3D(study, timestep = 0, do_scatter=False):
     not_visited = set()
     visited = set()
     node_vectors = {}
-    node_vectors_2d = {}
+    node_vectors_projection = {}
     links = {}
     colors = []
     #create the graph
@@ -186,10 +190,10 @@ def plot_study3D(study, timestep = 0, do_scatter=False):
         study, timestep, x_axis, y_axis, not_visited, node_vectors,
         node_vectors_2d, links)  
 
-    print(links)
+#     print(links)
     
     #collect plotting informatoin
-    lines, colors = dfs(g, node_vectors_2d, not_visited, visited, links, colors)
+    lines, color = dfs(g, node_vectors_2d, not_visited, visited, links, color)    
     
     xs = []
     ys = []
@@ -220,42 +224,16 @@ def plot_study(study, timestep = 0, x_axis=(1, 0, 0), y_axis=(0, 1, 0)):
     if is_normal(x_axis, y_axis) == False:
         raise pd3.Pd3Exception("Provided axes are not normal.")
 
-    not_visited = set()
-    visited = set()
-    node_vectors = {}
-    node_vectors_2d = {}
-    links = {}
-#   print(links)
+
     color = []
     #creating the graph
-    g = create_graph(study, timestep, x_axis, y_axis, not_visited, node_vectors, node_vectors_2d, links) 
+    g = Graph(study, timestep, x_axis, y_axis)
+
     
     #collect the information to plot
-    lines, color = dfs(g, node_vectors_2d, not_visited, visited, links, color)
-#     print(lines)
-#     print()
-    
-    
-#     for slip in colors:# go through colors and comapre the slip, if it equals each other, they get the same color if its different, it gets a different color
-
-#     print(links)
-#     print(color)
-    # Plots lines
-    # print(color)
-    """
-    final_colors = []
-    previous_slip = 0
-    for slip in color:
-        if slip == color[previous_slip]:
-            #slip.color = 'blue'
-            final_colors += slip
-            previous_slip = slip
-        else:
-            #slip.color = 'red'
-            final_colors += slip
-            previous_slip = slip
-    """
-    # replace color_lookup with this
+    lines, color = dfs(g, color)
+    print(lines)
+  # replace color_lookup with this
     # colors = [mcolors.to_rgba(c)
     #       for c in plt.rcParams['axes.prop_cycle'].by_key()['color']]
 
